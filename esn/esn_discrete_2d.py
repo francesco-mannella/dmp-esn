864fbf2b79a4a283adecd9c99721b6a24d5effb3
Francesco Mannella <francesco.mannella@gmail.com>
2016-03-08 14:19:15 +0100

    working on esn 2D task

--- esn/esn_discrete_2d.py
+++ esn/esn_discrete_2d.py
@@ -150,7 +150,218 @@ class ESN_discrete(object) :
         for t in range(self.timesteps):  
             y[t] = y_gen(t)
         return y 
+
+
+
+def get_angle(v1,v2) :
+    """
+    Calculate the angle between two vectors
+    v1  (array):   first vector
+    v2  (array):   second vector
+    """
+
+    if (norm(v1)*norm(v2)) != 0 :     
+        cosangle = dot(v2,v1)/(norm(v1)*norm(v2))
+        cosangle = maximum(-1,minimum(1, cosangle))
+        angle = arccos(cosangle)   
+        if cross(v2,v1) < 0 :
+            angle = 2*pi - angle  
+        return angle
+    return None
+
+TIMESTEPS = 200
+
+def gen_trajectory(start, goal, 
+                  ax=-0.5, bx=0.001, cx=.5, 
+                  ay=0.5,  by=0.001, cy=.48) :
+    '''
+    Generate an 'e' trajectory
     
+    start    (array): start point
+    goal     (array): end point
+    ax         float: amplitude of the x gaussian
+    bx         float: variance of the x gaussian
+    cx         float: mean of the x gaussian
+    ay         float: amplitude of the y gaussian
+    by         float: variance of the y gaussian
+    cy         float: mean of the y gaussian
+    '''
+    
+    # assure that these are np.arrays
+    start = np.array(start)
+    goal = np.array(goal)
+    
+    # the end point of the model trajectory
+    unit = [1,1]
+    # the end point of the real trajectory 
+    # translated to the origin
+    target = goal - start
+    
+    # he angle between model and real
+    angle = get_angle(unit, target) 
+    
+    # create model trajectory
+    t = np.linspace(0,1,TIMESTEPS)
+    xunit = ax*np.exp(-(1./bx)*(t-cx)**2)+t
+    yunit = ay*np.exp(-(1./by)*(t-cy)**2)+t
+    
+    # rotate
+    X = xunit*np.cos(angle) + yunit*np.sin(angle)
+    Y = -xunit*np.sin(angle) + yunit*np.cos(angle)
+    
+    # scale and traslate
+    scale = np.linalg.norm(target)/np.linalg.norm(unit)
+    X = X*scale + start[0]
+    Y = Y*scale + start[1]
+    
+    return X,Y,t
+
+
+
+def get_angle(v1,v2) :
+    """
+    Calculate the angle between two vectors
+    v1  (array):   first vector
+    v2  (array):   second vector
+    """
+
+    if (np.linalg.norm(v1)*np.linalg.norm(v2)) != 0 :     
+        cosangle = np.dot(v2,v1)/(np.linalg.norm(v1)*np.linalg.norm(v2))
+        cosangle = np.maximum(-1,np.minimum(1, cosangle))
+        angle = np.arccos(cosangle)   
+        if np.cross(v2,v1) < 0 :
+            angle = 2*np.pi - angle  
+        return angle
+    return None
+
+TIMESTEPS = 200
+
+def gen_trajectory(start, goal, 
+                  ax=-0.5, bx=0.001, cx=.5, 
+                  ay=0.5,  by=0.001, cy=.48) :
+    '''
+    Generate an 'e' trajectory
+    
+    start    (array): start point
+    goal     (array): end point
+    ax         float: amplitude of the x gaussian
+    bx         float: variance of the x gaussian
+    cx         float: mean of the x gaussian
+    ay         float: amplitude of the y gaussian
+    by         float: variance of the y gaussian
+    cy         float: mean of the y gaussian
+    '''
+    
+    # assure that these are np.arrays
+    start = np.array(start)
+    goal = np.array(goal)
+    
+    # the end point of the model trajectory
+    unit = [1,1]
+    # the end point of the real trajectory 
+    # translated to the origin
+    target = goal - start
+    
+    # he angle between model and real
+    angle = get_angle(unit, target) 
+    
+    # create model trajectory
+    t = np.linspace(0,1,TIMESTEPS)
+    xunit = ax*np.exp(-(1./bx)*(t-cx)**2)+t
+    yunit = ay*np.exp(-(1./by)*(t-cy)**2)+t
+    
+    # rotate
+    X = xunit*np.cos(angle) + yunit*np.sin(angle)
+    Y = -xunit*np.sin(angle) + yunit*np.cos(angle)
+    
+    # scale and traslate
+    scale = np.linalg.norm(target)/np.linalg.norm(unit)
+    X = X*scale + start[0]
+    Y = Y*scale + start[1]
+    
+    return X,Y,t
+
+
+r = 1.0
+xr = 0.0001
+NY = 9
+NT = NY-1
+angle_range = .8
+x = np.linspace(xr,-xr, NY)
+a = np.pi*((1.0-angle_range)*.5) +(np.pi*angle_range)*(1 -(x+xr)/(xr*2))
+rr = a/np.pi*r
+start_Y =[]
+goal_Y =[]
+for t in range(NY):
+    start_Y.append( [x[t], 0] )
+    goal_Y.append( [x[t] + rr[t]*np.cos(a[t]), rr[t]*np.sin(a[t])] )
+
+Y = []
+for k in range(NY):
+    x,y,t = gen_trajectory(start_Y[k], goal_Y[k])
+    Y.append(np.vstack((x,y)).T )
+
+
+dx = xr/float(NT)
+x = np.linspace(xr-dx,-xr+dx, NT)
+a = np.pi*((1.0-angle_range)*.5) +(np.pi*angle_range)*(1 -(x+xr)/(xr*2))
+rr = a/np.pi*r
+start_T =[]
+goal_T =[]
+for t in range(NT):
+    start_T.append( [x[t], 0] )
+    goal_T.append( [x[t] + rr[t]*np.cos(a[t]), rr[t]*np.sin(a[t])] )
+
+T = []
+for k in range(NT):
+    x,y,t = gen_trajectory(start_T[k], goal_T[k])
+    T.append(np.vstack((x,y)).T )
+    
+XLIM = (-xr-rr[-1]*1.2,xr+rr[1]*1.3)
+YLIM = (-rr[-1]*.5,rr[-1])
+
+try :
+    import matplotlib
+    import matplotlib.pyplot as plt
+
+    cc=matplotlib.colors.LinearSegmentedColormap.from_list(
+            'my_cm',[[0,0,1],[0,0,0],[0,.2,0],[0,0,0],[1,0,0]])
+    
+    def plot_t(x,y,t, xlim_=None, ylim_=None, lwidth = None) :
+        
+        start = (x[0],y[0])
+        goal = (x[-1],y[-1])
+        
+        if xlim_ == None: xlim_=[-1, 4]
+        if ylim_ == None: ylim_=[-1, 4]
+        if lwidth == None: lwidth=3
+            
+        plt.subplot(224)
+        plt.plot(x,t)
+        plt.xlim(xlim_)
+        
+        plt.subplot(221)
+        plt.plot(t,y)
+        plt.ylim(ylim_)
+        
+        plt.subplot(222)
+        for i in xrange(1,len(t)):
+            q = (i/float(len(t)))*255.0
+            plt.plot(x[(i-1):(i+1)],y[(i-1):(i+1)],lw=lwidth, color=cc(int(q)))
+        plt.plot(*start,marker='o', ms=lwidth*2, color='blue')
+        plt.plot(*goal,marker='o', ms=lwidth*2, color='red')
+        plt.xlim(xlim_)
+        plt.ylim(ylim_)
+    
+    def plot_e(start, goal) :
+        x,y,t = gen_trajectory(start, goal)
+        plot_t(x,y,t)
+     
+
+
+except :
+    print "no graphic device"
+
 if __name__ == "__main__" :
 
     sim = ESN_discrete()
